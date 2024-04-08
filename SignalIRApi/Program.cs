852a821aea3d03e4a7c430f89dd8bae1a83e656d
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-08 10:18:52 +0300

    About Apinin Test Edilmesi

--- SignalIRApi/Program.cs
+++ SignalIRApi/Program.cs
@@ -1,7 +1,15 @@
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SýgnalR.DataAccessLayer.Abstract;
+using SýgnalR.DataAccessLayer.Concrete;
+using SýgnalR.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<SignalRContext>();
+builder.Services.AddScoped<IAboutService, AboutManeger>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
