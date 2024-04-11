09286d184b4e76b855b496b239c898f562179edb
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-11 10:01:15 +0300

    Kategori Listelenmesi ve Eklenmesi

--- SignalIRWebUI/Controllers/CategoryController.cs
+++ SignalIRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalIRWebUI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace SignalIRWebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
         {
+            var client =_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44338/api/Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            createCategoryDto.Status = true;
+            var client=_httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44338/api/Category", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
---
+++ SignalIRWebUI/Dtos/CategoryDtos/GetCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalIRWebUI.Dtos.CategoryDtos
+{
+	public class GetCategoryDto
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
