c75e5f60c8652d40f1e5fdba3c0d3d775a4b6e99
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-11 10:36:29 +0300

    Silme Ve Güncelleme İşlemeleri

--- SignalIRApi/Controllers/CategoryController.cs
+++ SignalIRApi/Controllers/CategoryController.cs
@@ -13,14 +13,14 @@ namespace SignalIRApi.Controllers
     {
         private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
-        public CategoryController(ICategoryService categoryService,IMapper mapper)
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
             _mapper = mapper;
         }
 
         [HttpGet]
-        public IActionResult CategoryList() 
+        public IActionResult CategoryList()
         {
             var value = _mapper.Map<List<ResultCategoryDto>>(_categoryService.TGetListAll());
             return Ok(value);
@@ -31,18 +31,18 @@ namespace SignalIRApi.Controllers
             _categoryService.TAdd(new Category()
             {
                 CategoryName = crealteCategoryDto.CategoryName,
-                Status=true
+                Status = true
             });
             return Ok("Category Eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]//dışarıdan bir Id almalı
         public IActionResult DeleteCategory(int id) 
         {
             var value=_categoryService.TGetById(id);
             _categoryService.TDelete(value);
             return Ok("Category Silindi");
         }
-        [HttpGet("GetCategory")]
+        [HttpGet("{id}")]
         public IActionResult GetCategory(int id) 
         {
         
